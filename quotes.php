27a7e6f69692ef2a8cb042eccbe0d1f3ec532640
quarterdeck <56797103+quarterdeck@users.noreply.github.com>
2021-03-14 10:07:56 +0000

    Update quotes.php

--- quotes.php
+++ quotes.php
@@ -1,5 +1,6 @@
 <?php
 $quotes = [
+	["I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.","Bruce Lee"],
 	["Inspiration is for amateurs. The rest of us just show up and get to work. Every great idea I’ve ever had grew out of work itself.","Chuck Close"],
 	["Give me six hours to chop down a tree and I will spend the first four sharpening the axe.","Abraham Lincoln"],
 	["We are what we repeatedly do. Excellence, then, is not an act, but a habit.","Aristotle"],
