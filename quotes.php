81a6c77ac10c9863121dea52a7a579d0a330b1c8
quarterdeck <56797103+quarterdeck@users.noreply.github.com>
2021-03-16 10:00:05 +0000

    Update quotes.php

--- quotes.php
+++ quotes.php
@@ -1,5 +1,6 @@
 <?php
 $quotes = [
+	["We do not rise to the level of our expectations. We fall to the level of our training.","Archilochus"],
 	["Culture eats strategy for breakfast.","Peter Drucker"],
 	["In theory, theory and practice are the same. In practice they're not.","Unkown"],
 	["I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.","Bruce Lee"],
