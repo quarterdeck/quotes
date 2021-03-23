5771052125a3d771fb89405394e9d19e3ccceeba
quarterdeck <56797103+quarterdeck@users.noreply.github.com>
2021-03-23 09:46:01 +0000

    Update quotes.php

--- quotes.php
+++ quotes.php
@@ -1,5 +1,6 @@
 <?php
 $quotes = [
+	["Uneasy lies the head that wears a crown.","Shakespeare"],
 	["If you want to go fast, go alone. If you want to go far, go together.","African Proverb"],
 	["If you don't know where you're going, any road'll take you there.","George Harrison"],
 	["Amateurs talk about tactics, but professionals study logistics.","Gen. Robert H. Barrow"],
